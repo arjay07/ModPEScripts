2b0c8aa74bb54e5d13afb380fecfb5199f6b49f1
Leandro Yabut <arjay.yabut07@gmail.com>
2014-03-19 20:30:09 -0600

    Added some mods :P

---
+++ Climbing.js
@@ -0,0 +1,43 @@
+/*Climbing Mod*/
+
+var ls;
+
+function modTick(){
+
+	var blocks=1;
+	var x=Player.getX();
+	var z=Player.getZ();
+	var yaw=Player.getYaw();
+	var deltaX=Math.sin(yaw)*blocks;
+	var deltaZ=Math.cos(yaw)*blocks;
+	var block=getTile(Math.round(x+deltaX),Math.round(Player.getY()),Math.round(z+deltaZ));
+	
+	if(ls == 1){
+	
+		ls=0;
+		ModPE.saveData("LS", ls);
+		
+		if(block == 65){
+		
+			Level.setTile(Math.round(x+deltaX),Math.round(Player.getY()),Math.round(z+deltaZ), 0);
+		
+		}
+		
+	}
+
+	
+	if(block == 0){
+	
+		Level.setTile(Math.round(x+deltaX),Math.round(Player.getY()),Math.round(z+deltaZ), 65);
+		ls = 1;
+		ModPE.saveData("LS", ls);
+	
+	}
+	
+}
+
+function newLevel(){
+
+	ls = ModPE.readData("LS");
+
+}
