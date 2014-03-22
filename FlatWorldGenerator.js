60cc48f721e04ca12ae2888550464215babe8154
Leandro Yabut <arjay.yabut07@gmail.com>
2014-03-21 22:13:48 -0600

    Added new mods

---
+++ FlatWorldGenerator.js
@@ -0,0 +1,65 @@
+var speed = 10; //10 BPT (Blocks Per Tick)
+var material = 2; //The block the generator will use
+var posx = 0; //The positions will change
+var posy = 1;
+var posz = 0;
+var finished = false;
+var messagesent = false;
+
+function setSpeed(s){
+
+	speed = s;
+
+}
+
+function setMaterial(m){
+
+	material = m;
+
+}
+
+function modTick(){
+
+	if(!finished){
+	
+		for(var i = 0; i <= speed; i++){
+	
+			if(posy <= 4){
+	
+				Level.setTile(posx, posy, posz, 3);
+	
+			} else if(posy == 5){
+	
+				Level.setTile(posx, posy, posz, 2);
+	
+			} else {
+	
+				Level.setTile(posx, posy, posz, 0);
+	
+			}
+	
+			posx++;
+			posy++;
+			posz++;
+			
+			if(posx == 256 && posy == 128 && posz == 256){
+			
+				finished = true;
+				posx = 0;
+				posz = 0;
+				posy = 1;
+			
+			}
+	
+		}
+	
+	}
+	
+	if(finished && !messagesent){
+	
+		clientMessage("Finished");
+		messagesent = true;
+	
+	}
+
+}
---
+++ WorldGenerator.js
@@ -0,0 +1,70 @@
+/*World Generator*/
+
+var HILL = [1, 2, 3];
+
+function getSurface(x, y){
+
+	var y = 0;
+
+	for(var i = 0; i <= 128; i++){
+	
+		if(Level.getTile(x, i, z) == 0){
+		
+			y = i;
+			break;
+		
+		}
+	
+	}
+	
+	return y;
+
+}
+
+function layer(x1, z1, h, d, id, data){
+
+	for(var x = x1; x <= x1 + d; x++)
+		for(var z = z1; z <= z1 + w; z++){
+		
+			var y = getSurface(x, z);
+			
+			Level.setTile(x, y, z, id, data);
+		
+		}
+
+}
+
+function fillBelow(x, y, z, h, id, data){
+
+	for(var i = 0; i <= h; i++){
+	
+		if(Level.getTile(x, y - h, z) == 0){
+		
+			Level.setTile(x, y - h, z, id, data);
+		
+		}
+	
+	}
+
+}
+
+function createMaterialStructure(x, y, z, m, mdat){
+
+	var cubeX = [x, x+1, x+2];
+	var cubeY = [y, y+1, y+2];
+	var cubeZ = [z, z+1, z+2];
+	var amount = Math.floor((Math.random()*10));
+	
+	for(var i = 0; i < 3; i++){
+	
+		
+	
+	}
+
+}
+
+function spawnHill(x, y, z){
+
+
+
+}
