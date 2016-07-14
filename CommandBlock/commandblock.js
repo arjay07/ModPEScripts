2e3efffb7398eb45f630d76153f03ed3a2fb0bbc
Leandro Yabut <arjay.yabut07@gmail.com>
2016-07-14 17:19:16 -0600

    New Command Block Mod

---
+++ CommandBlock/commandblock.js
@@ -0,0 +1,27 @@
+/* Command Block Mod */
+/**
+* Copyright Arjay07
+* Version 1.0
+* 
+* This mod adds command blocks into the game while
+* it uses in-game redstone logic.
+*
+*/
+
+var commandblock = 137;
+
+function init(){
+
+	//Command Block
+    Block.defineBlock(commandblock, "Command Block", ["command_block", 0], 1, false, 0);
+    Block.setRedstoneConsumer(commandblock, true);
+    Block.setDestroyTime(commandblock, 0.3);
+    Block.setExplosionResistance(commandblock, 18000000);
+
+}
+
+function redstoneUpdateHook(x, y, z, newCurrent, idk, blockId, blockData){
+
+	if(blockId == 137) print("lel");
+
+}
